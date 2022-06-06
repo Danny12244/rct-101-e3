561dad1dc637a9395c637ba4335bcbf745f511d9
Danny12244 <pradeep.py725@gmail.com>
2022-06-06 17:30:01 +0530

    done

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -3,13 +3,15 @@ import { AuthContext } from "../context/AuthContext";
 
 const Login = () => {
   const[isAuth,login]=useContext(AuthContext)
+  const [email,setEmail]=useState("eve.holt@reqres.in")
+  const [password,setPassword]=useState("cityslicka")
   const submit= () => {
     login()
   }
   return (
     <div>
-      <input data-cy="login-email" placeholder="email"/><br/>
-      <input data-cy="login-password" placeholder="password"/><br/>
+      <input data-cy="login-email" type='email'value={email} onChange={({target})=>setEmail(target.value)} placeholder="email"/><br/>
+      <input data-cy="login-password" type='password'value={password} placeholder="password" onChange={({target})=>setPassword(target.value)}/><br/>
       <button data-cy="login-submit" onClick={submit}>Login</button>
     </div>
   );
